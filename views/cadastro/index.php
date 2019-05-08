0318dc06e8b32474f4f82621b583e86d62bcb882
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2019-05-08 09:34:05 -0300

    updates ministry's name

--- views/cadastro/index.php
+++ views/cadastro/index.php
@@ -40,7 +40,7 @@ $this->bodyProperties['ng-app'] = "culturaviva";
             <h2>Seja bem-vindo(a) à Rede Cultura Viva</h2>
             <p>Esta é a página do seu Ponto de Cultura. Apenas você tem acesso a ela.</p>
             <p>Fique a vontade para ir preenchendo as sessões. Você não precisa fazer tudo agora! Quando sua página estiver completa clique em "Enviar".</p>
-            <p>Depois, seu ponto poderá criar eventos, projetos e usar a plataforma para se manter em contato com o Ministério da Cultura.</p>
+            <p>Depois, seu ponto poderá criar eventos, projetos e usar a plataforma para se manter em contato com o Ministério da Cidadania.</p>
         </article>
         <div ng-if="agent_ponto.homologado_rcv" id="selo-index">
           <img id="selo-img" src="<?php $this->asset('img/verified-icon-big.png') ?>">
